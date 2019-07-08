200e54a8597d4910864bd2429174d6e9d59c0904
jbratcher <jeremybratcher@gmail.com>
2019-07-07 20:24:02 -0400

    menu: user input loop and exit case

--- Program.cs
+++ Program.cs
@@ -11,21 +11,13 @@ namespace TreePlanter
         static void Main(string[] args)
         {
 
-            // Create tree planting areas list from json file and PlantingArea class
-
             string currentDirectory = Directory.GetCurrentDirectory();
-            Console.WriteLine(currentDirectory);
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
             // Prepare planting areas data
             var plantingAreasJsonFile = Path.Combine(directory.FullName, "PlantingAreasData.json");
-            Console.WriteLine(plantingAreasJsonFile);
             // var <List<PlantingArea>>
             var areas = DeserializePlantingAreas(plantingAreasJsonFile);
-            foreach (var area in areas)
-            {
-                Console.WriteLine(area.ShortName);
-            }
 
             // modify data for display to user if display option chosen
 
@@ -33,70 +25,113 @@ namespace TreePlanter
             Console.WriteLine("Welcome to Tree Planter!\n");
             Console.WriteLine("Find or add places to plant trees in Louisville\n");
 
+
             // Display main menu and get user choice
             Console.WriteLine(string.Concat(Enumerable.Repeat("/", 43)));
             Console.WriteLine("// Enter your choice from the menu below //");
             Console.WriteLine("// 1. See available areas to plant       //");
-            Console.WriteLine("// 2. Add and area to plant              //");
+            Console.WriteLine("// 2. Add an area to plant               //");
+            Console.WriteLine("// 3. Edit an area to plant              //");
+            Console.WriteLine("// 3. Delete an area to plant            //");
+            Console.WriteLine("// 5. Quit application                   //");
             Console.WriteLine(string.Concat(Enumerable.Repeat("/", 43)));
 
-            // ask for and get user input
-            Console.WriteLine("Choice: ");
-            var input = Console.ReadLine();
-
-            // process choice
-            switch (Convert.ToInt32(input))
+            bool runapp = true;
+            int userInput = 0;
+            do
             {
-                case 1:
-                    // list available areas and details to user
-                    Console.WriteLine("Available Areas:\n");
-                    Console.WriteLine(String.Format("{0,-10} | {1,-11} | {2,5}", "Name", "Open Spaces", "Address"));
-                    foreach (var area in areas)
+
+                try
+                {
+
+                    // ask for and get user input
+                    Console.WriteLine("\nChoice: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                    // process choice
+                    switch (userInput)
                     {
-                        Console.WriteLine(String.Format("{0,-10} | {1,-11} | {2,5}", area.ShortName, area.OpenSpaces, area.Address));
+                        // list available areas and details to user 
+                        case 1:
+                            Console.WriteLine("Available Areas:\n");
+                            Console.WriteLine(String.Format("{0,-10} | {1,-11} | {2,5}", "Name", "Open Spaces", "Address"));
+                            foreach (var area in areas)
+                            {
+                                Console.WriteLine(String.Format("{0,-10} | {1,-11} | {2,5}", area.ShortName, area.OpenSpaces, area.Address));
+                            }
+                            break;
+
+                        // add a new planting area
+                        case 2:
+                            Console.WriteLine("Add an area");
+                            Console.WriteLine("___________");
+                            Console.WriteLine("Name: ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Address: ");
+                            string address = Console.ReadLine();
+                            Console.WriteLine("Spaces avaialbe: ");
+                            int spaces = Convert.ToInt32(Console.ReadLine());
+
+                            // make new obj with user input data
+                            PlantingArea newArea = new PlantingArea
+                            {
+                                UniqueID = areas.Count + 1,
+                                ShortName = name,
+                                Address = address,
+                                OpenSpaces = spaces,
+                                DateSubmitted = DateTime.Now
+
+                            };
+
+                            // add new planting area to list
+                            areas.Add(newArea);
+
+                            // save/serialize list back to file
+                            using (StreamWriter file = File.CreateText(plantingAreasJsonFile))
+                            {
+                                JsonSerializer serializer = new JsonSerializer();
+                                serializer.Serialize(file, areas);
+                            }
+
+                            break;
+
+                        case 3:
+                            // edit an area
+                            break;
+
+                        case 4:
+                            // delete an area
+                            break;
+
+                        case 5:
+                            runapp = false;
+                            // Exit script
+                            Console.WriteLine("Press any key to exit...\n");
+                            Console.ReadKey();
+                            break;
+
+                        default:
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(" Error: Invalid Choice");
+                            Console.ResetColor();
+                            System.Threading.Thread.Sleep(2000);
+                            break;
                     }
-                    break;
-                    
 
-                case 2:
-                    Console.WriteLine("Add an area");
-                    // capture user input
-                    Console.WriteLine("Enter the area name: "); 
-                    string name = Console.ReadLine();
-                    Console.WriteLine("Enter the area address: ");
-                    string address = Console.ReadLine();
-                    Console.WriteLine("Enter the number of spaces avaialbe: ");
-                    int spaces = Convert.ToInt32(Console.ReadLine());
-                    // make new obj with input data
-                    PlantingArea newArea = new PlantingArea
-                    {
-                        UniqueID = areas.Count + 1,
-                        ShortName = name,
-                        Address = address,
-                        OpenSpaces = spaces,
-                        DateSubmitted = DateTime.Now
+                } catch(Exception e)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" Unexpected Error:");
+                    Console.WriteLine(e);
+                    Console.ResetColor();
+                    System.Threading.Thread.Sleep(2000);
+                }
 
-                    };
-                    // add new planting area to list
-                    areas.Add(newArea);
-                    Console.WriteLine(areas.Count);
-                    // save list back to file
-                    using (StreamWriter file = File.CreateText(plantingAreasJsonFile))
-                    {
-                        JsonSerializer serializer = new JsonSerializer();
-                        serializer.Serialize(file, areas);
-                    }
+            } while (runapp != false);
 
-                    break;
-                    
-            }
-
-            // Exit script
-            Console.WriteLine("Press any key to exit...\n");
-            Console.ReadKey();
-
-
-        }
+        }       
 
         // Deserialize json data
 
